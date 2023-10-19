8ca9c709aefcc09d0d1ee18feee65e20b30e02eb
prawee wongsa <xxx>
2023-10-19 10:41:46 +0700

    decode mobile data

--- src/api/contact/content-types/contact/lifecycles.js
+++ src/api/contact/content-types/contact/lifecycles.js
@@ -4,4 +4,8 @@ module.exports = {
         // event.params.data.mobile = 'dddddddddd'
         event.params.data.mobile = btoa(event.params.data.mobile)
     },
+    async afterFindOne(event) {
+        // console.log('afterFindOne ', event.result)
+        event.result.mobile = atob(event.result.mobile)
+    },
 };
